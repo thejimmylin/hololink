d083ebe8e2ce78ceedb0fbb74c354f0ef54e6af3
Jimmy Lin <64210965+j3ygithub@users.noreply.github.com>
2020-06-01 17:33:22 +0800

    added a landpage template or something like that

--- hololink/hololink/views.py
+++ hololink/hololink/views.py
@@ -1,6 +1,59 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from accounts.forms import SignUpWithEmailForm
+from django.core.mail import send_mail
+import uuid
+
+
+# a email-sending script, not a view
+def send_password_email(subject, message, recipient):
+    try:
+        send_mail(
+            subject=subject,
+            message=message,
+            from_email=settings.DEFAULT_FROM_EMAIL,
+            recipient_list=[recipient],
+            fail_silently=False,
+        )
+    except:
+        pass
 
 
 def index(request):
-    context = {}
+    if request.method == 'POST': 
+        form = SignUpWithEmailForm(request.POST) 
+        if form.is_valid():
+            user = form.save(commit=False)
+            email = form.cleaned_data.get('email')
+            username = email[0:email.index('@')]
+            user.username = username
+            random_uuid_password = uuid.uuid4().hex[0:6]
+            user.set_password(random_uuid_password)
+            user.save()
+            # send a random uuid password email
+            recipient = f'{email}'
+            subject = "[Hololink] You have created an account."
+            try:
+                if '_' in username:
+                    username_readable = ' '.join([ word[0].upper() + word[1:] for word in username.split('_') ])
+                else:
+                    username_readable = username
+            except:
+                username_readable = username
+            message = f'Hi {username_readable},'
+            message += '\n\nYou have created a new account on Hololink. You could login and change it on Hololink later.'
+            message += f'\n\nYour account: {username}'
+            message += f'\nYour password: {random_uuid_password}'
+            message += '\n\nSincerely,'
+            message += '\nHololink'
+            send_password_email(
+                subject=subject,
+                message=message,
+                recipient=recipient,
+            )
+            return redirect('/accounts/password_reset/done/')
+    else: 
+        form = SignUpWithEmailForm()
+    context = {
+        'form': form,
+    }
     return render(request, 'index.html', context)
