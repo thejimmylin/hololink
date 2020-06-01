42cfa1d507c46ccc8467455262a7b15f5752612d
Jimmy Lin <64210965+j3ygithub@users.noreply.github.com>
2020-06-01 15:20:12 +0800

    added index.html and a navbar dropdown containing article, account

--- hololink/hololink/urls.py
+++ hololink/hololink/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
 from django.views.generic import RedirectView
 from django.contrib import admin
+from .views import index
 
 urlpatterns = [
     path('i18n/', include('django.conf.urls.i18n')),
@@ -8,5 +9,5 @@ urlpatterns = [
     path('accounts/', include('accounts.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
     path('article/', include('article.urls')),
-    path('', RedirectView.as_view(url='/article/')),
+    path('', index, name='index'),
 ]
